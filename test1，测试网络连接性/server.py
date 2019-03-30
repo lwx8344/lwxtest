88468f92673ffecd29bc7a98e18ff3fd1fea7fa5
lwx8344 <45914622+lwx8344@users.noreply.github.com>
2019-03-30 13:27:41 +0800

    Update server.py

--- test1，测试网络连接性/server.py
+++ test1，测试网络连接性/server.py
@@ -1,1 +1,17 @@
+# coding=utf-8
+#椅子1测试程序
+import socket
 address = ('0.0.0.0',9996)#0000表示接收所有ip地址发过来的消息
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(address)
+
+
+while 1:
+    data,address=s.recvfrom(2048)
+    if not data:
+        break
+    print("got data from",address)
+    print(data.decode())
+    if data.decode()=="happy":
+        print("runrurnrun!")
+s.close()
